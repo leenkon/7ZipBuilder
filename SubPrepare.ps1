6f36dede3a5ff4936cd940f9a260b32705362dcc
leenkon <leenkon@qq.com>
2025-08-06 20:35:40 +0800

    Update SubPrepare.ps1

--- SubPrepare.ps1
+++ SubPrepare.ps1
@@ -1,19 +1,20 @@
 param(
     [Parameter(Mandatory = $true, ValueFromPipeline = $true)]
-    [string] $BuildDirectory,
+    [string] $buildDir,
     [Parameter(Mandatory = $true, ValueFromPipeline = $true)]
     [string] $BuildVersion
 )
 
 $workDir = $PSScriptRoot
 $resDir = "$workDir\..\Resources"
+$buildDir = "$workDir\$BuildVersion"
 
 # 拷贝资源文件
-Copy-Item -Force -Path "$resDir\Format7zF.rc" -Destination "$BuildDirectory\CPP\7zip\Bundles\Format7zF\resource.rc"
-Copy-Item -Force -Path "$resDir\Fm.rc" -Destination "$BuildDirectory\CPP\7zip\Bundles\Fm\resource.rc"
+Copy-Item -Force -Path "$resDir\Format7zF.rc" -Destination "$buildDir\CPP\7zip\Bundles\Format7zF\resource.rc"
+Copy-Item -Force -Path "$resDir\Fm.rc" -Destination "$buildDir\CPP\7zip\Bundles\Fm\resource.rc"
 
 # 拷贝UI图
-Copy-Item -Force -Path "$resDir\ToolBarIcons\*.bmp" -Destination "$BuildDirectory\CPP\7zip\UI\FileManager"
+Copy-Item -Force -Path "$resDir\ToolBarIcons\*.bmp" -Destination "$buildDir\CPP\7zip\UI\FileManager"
 
 # 拷贝图标
-Copy-Item -Force -Recurse -Path "$resDir\FileIcons\*.ico" -Destination "$BuildDirectory\CPP\7zip\Archive\Icons"
+Copy-Item -Force -Recurse -Path "$resDir\FileIcons\*.ico" -Destination "$buildDir\CPP\7zip\Archive\Icons"
