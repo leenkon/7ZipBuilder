b6f7ce72e63f20e45dbbe5de333dff7ddac0e7e5
leenkon <leenkon@qq.com>
2025-08-06 18:47:45 +0800

    Update SubPrepare.ps1

--- SubPrepare.ps1
+++ SubPrepare.ps1
@@ -1,8 +1,13 @@
-param(
-    [Parameter(Mandatory = $true, ValueFromPipeline = $true)]
-    [string] $BuildDirectory,
-    [Parameter(Mandatory = $true, ValueFromPipeline = $true)]
-    [string] $BuildVersion
-)
 
-Copy-Item -Force -Recurse -Path "FileIcons\*.ico" -Destination "$BuildDirectory\CPP\7zip\Archive\Icons"
+$workDir = $PSScriptRoot
+$resDir = "$workDir\..\Resources"
+
+# 拷贝图标
+Copy-Item -Force -Recurse -Path "$resDir\FileIcons\*.ico" -Destination "$BuildDirectory\CPP\7zip\Archive\Icons"
+
+# 拷贝资源文件
+Copy-Item -Force -Path "$resDir\Format7zF.rc" -Destination "$BuildDirectory\CPP\7zip\Bundles\Format7zF\resource.rc"
+Copy-Item -Force -Path "$resDir\Fm.rc" -Destination "$BuildDirectory\CPP\7zip\Bundles\Fm\resource.rc"
+
+# 拷贝UI图
+Copy-Item -Force -Path "$resDir\ToolBarIcons\*.bmp" -Destination "$BuildDirectory\CPP\7zip\UI\FileManager"
